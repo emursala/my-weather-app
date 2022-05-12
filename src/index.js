4a33d46977132e03597925c6e05ba72930181d83
emursala <101676056+emursala@users.noreply.github.com>
2022-05-12 09:42:12 -0700

    added weather forecast to JS & updated CSS+HTML

--- src/index.js
+++ src/index.js
@@ -20,12 +20,20 @@ function weather(response) {
   );
   document.querySelector("#cloudy").innerHTML =
     response.data.weather[0].description;
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
   let apiKey = "993af96e9543b078265c47b2bab8efae";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(weather);
+}
+
+function getForecast(coordinates) {
+  let apiKey = "993af96e9543b078265c47b2bab8efae";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 let now = new Date();
@@ -56,6 +64,52 @@ function citySearch(event) {
   event.preventDefault();
   let city = document.querySelector("#change-city").value;
   search(city);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<table class="table">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<thead>
+    </thead>
+    <tbody>
+    <tr>
+    <th scope="row"></th>
+    <td><strong>${formatDay(forecastDay.dt)}</strong></td>
+    <td>
+    <img class="forecastIcons"src="http://openweathermap.org/img/wn/${
+      forecastDay.weather[0].icon
+    }@2x.png">
+    </td>
+    <td>H: ${Math.round(forecastDay.temp.max)}° L: ${Math.round(
+          forecastDay.temp.min
+        )}°</td>
+    </tr> </tbody>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</table>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 let formInput = document.querySelector("#search-form");
